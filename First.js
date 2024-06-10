0aa7130a1f86ca2923c5fd527459fd539ab14327
Aqdas Ali <aqdasali584@gmail.com>
2024-06-10 21:02:15 +0500

    First Class

---
+++ First.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+var name = "Aqdas Ali";
+console.log("My Name IS :", name);
+var x = 7;
+var y = 6;
+var sum = x + y;
+console.log("Sum IS ", sum);
+
+//variables
+var firstName = "Aqdas";
+var lastName = "Ali";
+console.log(firstName + " " + lastName);
+
+//variable and declaration rules
+//1- Variable name must not starts with numbers
+//2- Variable name must not starts and ends with operators
